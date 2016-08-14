ab853f66766f82d8a44200ee0c1e3eed009a7f50
pokutuna <popopopopokutuna@gmail.com>
2016-08-15 02:48:40 +0900

    use webNavigation

--- src/background.ts
+++ src/background.ts
@@ -1,10 +1,8 @@
 /// <reference path="../typings/index.d.ts" />
 
-chrome.tabs.onUpdated.addListener(function (tabId: number, changeInfo: chrome.tabs.TabChangeInfo, tab: chrome.tabs.Tab) {
-   if (changeInfo.status === 'complete') {
-       if (true) { // TODO
-           chrome.tabs.executeScript(tabId, { file: 'js/content.js' });
-           chrome.tabs.insertCSS(tabId, { file: 'css/content.css' });
-       }
+chrome.webNavigation.onDOMContentLoaded.addListener((details: chrome.webNavigation.WebNavigationFramedCallbackDetails) => {
+    if (true) { // TODO check config
+        chrome.tabs.executeScript(details.tabId, { file: 'js/content.js' });
+        chrome.tabs.insertCSS(details.tabId, { file: 'css/content.css' });
     }
 });
